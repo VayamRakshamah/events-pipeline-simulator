db05f41a2377483b453e8e3dbe35f26cb0281b9c
Vinay Gupta <vinaygupta890@gmail.com>
2026-06-22 23:30:26 +0530

    chore: scaffold event pipeline simulator workspace

---
+++ backend/src/main/java/in/vinaygupta/eventpipeline/EventPipelineSimulatorApplication.java
@@ -0,0 +1,11 @@
+package in.vinaygupta.eventpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPipelineSimulatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventPipelineSimulatorApplication.class, args);
+    }
+}
